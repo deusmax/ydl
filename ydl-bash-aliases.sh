0919b3d6dfc3510c0bca3b6ec4b00f74657531fe
Deus Max <deusmax@gmx.com>
2019-02-25 00:13:02 +0200

    minor edits and file renaming

--- ydl-bash-aliases.sh
+++ ydl-bash-aliases.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 # ydl
 if [ -x /usr/local/bin/youtube-dl ] ; then
     alias ydl-cmd='youtube-dl --restrict-filenames --no-mtime'
     alias ydl-mp4='youtube-dl --restrict-filenames --no-mtime -f 18'
     alias ydl-m4a='youtube-dl --restrict-filenames --no-mtime -f 140'
-    alias ydl-video='youtube-dl --restrict-filenames --no-mtime -f 43'
-    alias ydl-audio='youtube-dl --restrict-filenames --no-mtime -f 171'
+    alias ydl-video='youtube-dl --restrict-filenames --no-mtime -f 43'   # webm
+    alias ydl-audio='youtube-dl --restrict-filenames --no-mtime -f 171'  # webm
 fi
 
 if [ -x  /usr/bin/ffmpeg ] ; then
-    VOLUME2_DIR="~/Downloads/youtube/volume2"
+    VOLUME2_DIR=~/Downloads/youtube/volume2
     function v2-video () {
         ffmpeg -v warning -i $1 -c:v copy -af volume=2.2 $VOLUME2_DIR/$(basename $1)
     }
